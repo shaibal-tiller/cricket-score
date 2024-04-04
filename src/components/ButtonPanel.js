d63adf085fba04b81adac9638403e7aa4f1ecff9
shaibal-tiller <shaibal.tiller@gmail.com>
2024-04-04 16:39:13 +0600

    match structure

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -14,12 +14,20 @@ const ButtonPanel = ({ setter, }) => {
     const [run_2, setRun_2] = useState(currentScore_2?.runs || 0)
     const [wicket_2, setWicket_2] = useState(currentScore_2?.wickets || 0)
 
+    const [progressStart, setProgressStart] = useState(0);
+    const [progressInterval, setProgressInterval] = useState(null);
+
+
     useEffect(() => {
-        innings == '1st Innings' ? localStorage.setItem("currentScore_1", JSON.stringify({ balls: ball, runs: run, wickets: wicket }))
-            : localStorage.setItem("currentScore_2", JSON.stringify({ balls: ball_2, runs: run_2, wickets: wicket_2 }))
+        const hudai = (innings == '1st Innings') ?
+            localStorage.setItem("currentScore_1", JSON.stringify({ balls: ball, runs: run, wickets: wicket })) :
+            (innings == '2nd Innings') ? localStorage.setItem("currentScore_2", JSON.stringify({ balls: ball_2, runs: run_2, wickets: wicket_2 }))
+                : 0
 
         window.dispatchEvent(new Event('storage'))
-    }, [ball, run, wicket])
+    }, [ball, run, wicket, ball_2, run_2, wicket_2])
+
+
 
     const handleNewMatch = (e) => {
         const isConfirm = window.confirm("Are you Sure?\n Data will be lost")
@@ -30,21 +38,35 @@ const ButtonPanel = ({ setter, }) => {
     }
 
     const update_ball_count = () => {
-        innings == '1st Innings' ? setBall(ball + 1)
-            : setBall_2(ball + 1)
+        const hudai = (innings == '1st Innings') ? setBall(ball + 1)
+            : (innings == '2nd Innings') ? setBall_2(ball_2 + 1) : 0
 
     }
     const update_runs = (runs) => {
-        innings == '1st Innings' ? setRun(run + runs)
-            : setRun_2(run + runs)
+        const hudai = (innings == '1st Innings') ? setRun(run + runs)
+            : (innings == '2nd Innings') ? setRun_2(run_2 + runs) : 0
     }
     const update_wicket = () => {
-        innings == '1st Innings' ? setWicket(wicket + 1)
-            : setWicket_2(wicket + 1)
+        const hudai = (innings == '1st Innings') ? setWicket(wicket + 1)
+            : (innings == '2nd Innings') ? setWicket_2(wicket_2 + 1) : 0
     }
 
-    const handleScore = (e, action) => {
-        e.preventDefault()
+    const handleLongPressStart = (action) => {
+
+        setProgressStart(new Date().getTime())
+
+    };
+
+    const handleLongPressEnd = (action) => {
+
+        if (new Date().getTime() - progressStart>1250) {
+            handleScore(action);
+        }
+
+    };
+
+    const handleScore = (action) => {
+
         switch (action) {
             case 'dot':
                 update_ball_count()
@@ -79,28 +101,63 @@ const ButtonPanel = ({ setter, }) => {
 
 
         }
+        navigator.vibrate(200); // Vibrate after action is completed
     }
     return (
-        <div className='px-2 h-full text-[2rem] text-center space-y-2 text-white'>
-            <div onClick={(e) => handleScore(e, 'wicket')} className=' active:scale-95 shadow-lg text-5xl font-bold bg-yellow-400'>W</div>
-            <div onClick={(e) => handleScore(e, 'dead-ball')} className=' active:scale-95 shadow-lg bg-gray-600 '>DB</div>
-            <div onClick={(e) => handleScore(e, 'wide-ball')} className=' active:scale-95 shadow-lg bg-gray-600 '>Wd</div>
-            <div className='  flex justify-between gap-4 '>
-                <div className='   w-full space-y-2'>
-                    <div onClick={(e) => handleScore(e, 'no-ball')} className='active:scale-95 shadow-lg bg-yellow-200'>
+        <div className='px-4 h-full text-[2rem] text-center space-y-2 text-white '>
+            <div onMouseDown={() => handleLongPressStart('wicket')}
+                onTouchEnd={() => handleLongPressEnd('wicket')}
+                onMouseUp={handleLongPressEnd}
+                onTouchStart={handleLongPressStart} className='active:border-black border-2 border-transparent select-none rounded-lg active:scale-95 shadow-lg text-5xl font-bold bg-yellow-400'>W</div>
+          
+            <div onMouseUp={handleLongPressEnd}
+                onTouchStart={handleLongPressStart}
+                onMouseDown={() => handleLongPressStart('dead-ball')}
+                onTouchEnd={() => handleLongPressEnd('dead-ball')} className='active:border-black border-2 border-transparent select-none rounded-lg active:scale-95 shadow-lg bg-gray-600 '>DB</div>
+            
+            <div className='flex items-center w-full gap-4'>
+                <div onMouseUp={handleLongPressEnd}
+                    onTouchStart={handleLongPressStart}
+                    onTouchEnd={() => handleLongPressEnd('wide-ball')}
+                    onMouseDown={() => handleLongPressStart('wide-ball')} className='active:border-black border-2 border-transparent select-none rounded-lg active:scale-95 shadow-lg bg-gray-600 w-[50%]'>Wd</div>
+                <div onMouseUp={handleLongPressEnd}
+                    onTouchStart={handleLongPressStart}
+                    onTouchEnd={() => handleLongPressEnd('wide-ball-w')}
+                    onMouseDown={() => handleLongPressStart('wide-ball-w')} className='active:border-black border-2 border-transparent select-none rounded-lg active:scale-95 shadow-lg bg-gray-600 w-[50%]'>Wd + W</div>
+            </div>
+            <div className='  flex justify-between gap-3 items-center '>
+                <div className=' flex items-center  w-[75%]  gap-2'>
+                    <div onMouseUp={handleLongPressEnd}
+                        onTouchStart={handleLongPressStart}
+                        onTouchEnd={() => handleLongPressEnd('no-ball')}
+                        onMouseDown={() => handleLongPressStart('no-ball')} className='active:border-black border-2 border-transparent select-none rounded-lg active:scale-95 shadow-lg bg-yellow-200 py-6 w-[20%]' >
                         NB
                     </div>
-                    <div onClick={(e) => handleScore(e, 'no-ball-4')} className=' active:scale-95 shadow-lg bg-orange-400'>
+                    <div onMouseUp={handleLongPressEnd}
+                        onTouchStart={handleLongPressStart}
+                        onTouchEnd={() => handleLongPressEnd('no-ball-4')}
+                        onMouseDown={() => handleLongPressStart('no-ball-4')} className='active:border-black border-2 border-transparent select-none rounded-lg active:scale-95 shadow-lg bg-orange-400 py-6 w-[40%]'>
                         NB+4
                     </div>
-                    <div onClick={(e) => handleScore(e, 'no-ball-w')} className=' active:scale-95 shadow-lg bg-orange-400'>
+                    <div onMouseUp={handleLongPressEnd}
+                        onTouchStart={handleLongPressStart}
+                        onTouchEnd={() => handleLongPressEnd('no-ball-w')}
+                        onMouseDown={() => handleLongPressStart('no-ball-w')} className='active:border-black border-2 border-transparent select-none rounded-lg active:scale-95 shadow-lg bg-orange-400 py-6 w-[40%]'>
                         NB+W
                     </div>
                 </div>
-                <div onClick={(e) => handleScore(e, '4')} className=' active:scale-95 shadow-lg w-full bg-orange-400 text-center text-5xl font-bold py-6'>4</div>
+                <div onMouseUp={handleLongPressEnd}
+                    onTouchStart={handleLongPressStart}
+                    onTouchEnd={() => handleLongPressEnd('4')}
+                    onMouseDown={() => handleLongPressStart('4')}
+                    className='active:border-black  border-2 border-transparent  rounded-lg active:scale-95 shadow-lg w-[25%] bg-orange-400 text-center
+                 text-5xl font-bold py-6 '>4</div>
             </div>
-            <div onClick={(e) => handleScore(e, 'dot')} className=' active:scale-95 shadow-lg w-full px-4 bg-orange-300 min-h-8 mt-auto py-4 font-bold text-5xl'>Dot</div>
-            <div onClick={handleNewMatch} className=' active:scale-95 shadow-lg mx-auto w-[80%] px-4 bg-orange-800 min-h-8 py-2 !mt-10  font-bold text-3xl'>RESET</div>
+            <div onMouseUp={handleLongPressEnd}
+                onTouchStart={handleLongPressStart}
+                onTouchEnd={() => handleLongPressEnd('dot')}
+                onMouseDown={() => handleLongPressStart('dot')} className='active:border-black border-2 border-transparent select-none rounded-lg active:scale-95 shadow-lg w-full px-4 bg-orange-300 min-h-8 mt-auto py-4 font-bold text-5xl'>Dot</div>
+            <div onClick={handleNewMatch} className='rounded-lg active:scale-95 shadow-lg mx-auto w-[80%] px-4 bg-orange-800 min-h-8 py-2 !mt-10  font-bold text-3xl'>RESET</div>
         </div>
     )
 }
--- src/components/Display.js
+++ src/components/Display.js
@@ -11,8 +11,12 @@ const Display = () => {
   const commentary = localStorage.getItem('commentary')
   const [currentScore_1, setCurrentScore_1] = useState(JSON.parse(localStorage.getItem('currentScore_1')) || {});
   const [currentScore_2, setCurrentScore_2] = useState(JSON.parse(localStorage.getItem('currentScore_2')) || {});
+  const [is_match_over, set_is_match_over] = useState(false)
 
+  const showResult = (e) => {
+    set_is_match_over(true)
 
+  }
   const updateScore = () => {
     const score1 = JSON.parse(localStorage.getItem('currentScore_1')) || {};
     setCurrentScore_1(score1);
@@ -26,13 +30,43 @@ const Display = () => {
   }, []);
 
   useEffect(() => {
-    if ((currentScore_1?.wickets == max_wicket - 1) ||
-      (currentScore_1.balls == max_over * balls_per_over)) {
-      localStorage.setItem('innings2', 'playing')
-      localStorage.setItem('innings1', JSON.stringify({ currentScore_1: currentScore_1, commentary: commentary }))
-      localStorage.setItem('currentScore_1', JSON.stringify({ runs: 0, balls: 0, wickets: 0 }))
+
+    if (innings == '1st Innings') {
+      if (currentScore_1?.wickets == max_wicket - 1) {
+        localStorage.setItem('innings2', 'playing')
+        localStorage.setItem('innings1', "all-out")
+      }
+      else if ((currentScore_1.balls == (max_over * balls_per_over))) {
+        console.log(currentScore_1.balls);
+        localStorage.setItem('innings2', 'playing')
+        localStorage.setItem('innings1', "over")
+      }
     }
   }, [currentScore_1])
+
+  useEffect(() => {
+    if (innings == '2nd Innings') {
+      const target = JSON.parse(localStorage.getItem('currentScore_1')).runs
+
+      if (target <= currentScore_2.runs) {
+        localStorage.setItem('innings2', 'won')
+        localStorage.setItem('innings1', 'lost')
+        showResult()
+      }
+      else if (currentScore_2?.wickets == max_wicket - 1) {
+
+        localStorage.setItem('innings2', 'lost')
+        localStorage.setItem('innings1', 'won')
+        showResult()
+      }
+      else if ((currentScore_2.balls == (max_over * balls_per_over))) {
+
+        localStorage.setItem('innings2', 'lost')
+        localStorage.setItem('innings1', 'won')
+        showResult()
+      }
+    }
+  }, [currentScore_2])
 
   // useEffect(() => {
   //   if ((currentScore?.wickets == max_wicket - 1) ||
@@ -46,17 +80,24 @@ const Display = () => {
 
   return (
     <div className='h-[20vh] m-4 bg-[#438486d0] rounded-lg text-white  px-2'>
-      <p className='font-bold tracking-widest'> {innings}  </p>
-      {innings == '1st Innings' ? <div className='flex justify-between'>
+      {!is_match_over ?
+        (<div>
+          <p className='font-bold tracking-widest'> {innings}  </p>
+          {innings == '1st Innings' ? <div className='flex justify-between'>
 
-        <h2 className='text-[42px]'><span className='text-sm'>runs </span>{currentScore_1?.runs} <span className='text-sm'>for </span>{currentScore_1?.wickets}</h2>
-        <h3 className='text-[42px] float-right'><span className='text-xl'>Overs: </span> {parseInt(currentScore_1?.balls / balls_per_over)}.{currentScore_1?.balls % balls_per_over}</h3>
-      </div> :
-        <div className='flex justify-between'>
-          <h2 className='text-[42px]'><span className='text-sm'>runs </span>{currentScore_2?.runs} <span className='text-sm'>for </span>{currentScore_2?.wickets}</h2>
-          <h3 className='text-[42px] float-right'><span className='text-xl'>Overs: </span> {parseInt(currentScore_2?.balls / balls_per_over)}.{currentScore_2?.balls % balls_per_over}</h3>
+            <h2 className='text-[42px]'><span className='text-sm'>runs </span>{currentScore_1?.runs} <span className='text-sm'>for </span>{currentScore_1?.wickets}</h2>
+            <h3 className='text-[42px] float-right'><span className='text-xl'>Overs: </span> {parseInt(currentScore_1?.balls / balls_per_over)}.{currentScore_1?.balls % balls_per_over}</h3>
+          </div> :
+            <div className='flex justify-between'>
+              <h2 className='text-[42px]'><span className='text-sm'>runs </span>{currentScore_2?.runs} <span className='text-sm'>for </span>{currentScore_2?.wickets}</h2>
+              <h3 className='text-[42px] float-right'><span className='text-xl'>Overs: </span> {parseInt(currentScore_2?.balls / balls_per_over)}.{currentScore_2?.balls % balls_per_over}</h3>
+            </div>}
+        </div>)
+        :
+        <div className='text-center py-4 text-3xl'> 
+          <p>Match Ended</p>
+          <p>Team 2 <span className='font-semibold capitalize'>{localStorage.getItem('innings2')}</span></p>
         </div>}
-      <p></p>
 
     </div>
   );
